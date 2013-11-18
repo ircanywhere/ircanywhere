8a780a2a1e6f6d530b0bec0ee4ef82c3d8a7f620
rickihastings <rickihastings22@gmail.com>
2013-11-18 22:03:26 +0000

    Added user registration
    This is done with Accounts.createUser() and done on the server side to secure it

--- client/views/home.js
+++ client/views/home.js
@@ -1,17 +1,23 @@
-var focusEmail = function (e, t) {
+Template.login.events({
+	'focus input#login-email': function (e, t) {
 		if (e.target.value == 'Email Address')
 			e.target.value = '';
 	},
-	blurEmail = function (e, t) {
+
+	'focus input#reset-email': function (e, t) {
+		if (e.target.value == 'Email Address')
+			e.target.value = '';
+	},
+
+	'blur input#login-email': function (e, t) {
 		if (e.target.value == '')
 			e.target.value = 'Email Address';
-	};
+	},
 
-Template.login.events({
-	'focus input#login-email': focusEmail,
-	'focus input#reset-email': focusEmail,
-	'blur input#login-email': blurEmail,
-	'blur input#reset-email': blurEmail,
+	'blur input#reset-email': function (e, t) {
+		if (e.target.value == '')
+			e.target.value = 'Email Address';
+	},
 
 	'focus input#login-password': function (e, t) {
 		if (e.target.value == 'Password')
@@ -24,7 +30,7 @@ Template.login.events({
 	},
 
 	'click a#forgot-password-link': function (e, t) {
-		$('#forgot-password,#forgot-password-actions').toggle();
+		$('#forgot-password,#forgot-password-actions').slideToggle('fast');
 
 		return false;
 	},
@@ -57,6 +63,26 @@ Template.login.events({
 	}
 });
 
+Template.signup.yourName = function(t) {
+	return Session.get('signup.yourName');
+};
+
+Template.signup.ircNickname = function(t) {
+	return Session.get('signup.ircNickname');
+};
+
+Template.signup.emailAddress = function(t) {
+	return Session.get('signup.emailAddress');
+};
+
+Template.signup.errors = function(t) {
+	return Session.get('signup.errors');
+};
+
+Template.signup.success = function(t) {
+	return Session.get('signup.success');
+};
+
 Template.signup.events({
 	'submit form#signup-form': function(e, t) {
 		e.preventDefault();
@@ -64,10 +90,22 @@ Template.signup.events({
 		var name = t.find('#your-name').value,
 			nickname = t.find('#irc-nickname').value,
 			email = t.find('#email-address').value,
-			password = t.find('#password').value;
+			password = t.find('#password').value,
+			confirmPassword = t.find('#confirm-password').value,
+			errors = [];
+		// grab all our variables
 
-		var email = Helpers.trimInput(email);
-
-		
+		Meteor.call('registerUser', name, nickname, email, password, confirmPassword, function(err, result) {
+			if (result.failed) {
+				Session.set('signup.yourName', name);
+				Session.set('signup.ircNickname', nickname);
+				Session.set('signup.emailAddress', email);
+				Session.set('signup.errors', result.errors);
+				// reset some fields to keep our user happy
+			} else {
+				Session.set('signup.errors', []);
+				Session.set('signup.success', 'Your account has been sucessfully created, you will recieve an email shortly');
+			}
+		});
 	}
 });
--- server/main.js
+++ server/main.js
@@ -1,4 +1,9 @@
+Accounts.config({
+	sendVerificationEmail: false,
+	forbidClientAccountCreation: true
+	// set our default accounts config
+});
+
 Meteor.startup(function () {
 	// code to run on server at startup
-	
 });
