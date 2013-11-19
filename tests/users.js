f8167f729a909fb5d72bd0447f3c90930ea213d8
rickihastings <rickihastings22@gmail.com>
2013-11-19 16:34:34 +0000

    Added some tests for registerUser

---
+++ tests/users.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+
+suite('Users', function() {
+	test('Users.registerUser', function (done, server, client) {
+		server.eval(function() {
+			Meteor.call('registerUser', 'John Smith', 'john.smith@aol.com', '123456', '123456');
+
+			emit('done');
+		}).once('done', function() {
+			done();
+		});
+
+		client.eval(function() {
+			Meteor.loginWithPassword('john.smith@aol.com', '123456', function() {
+				Posts.insert({title: 'hello'});
+			});
+		});
+	});
+});
