5a2594a102105020a50da87e1eefb9e343e6f4f3
Ricki Hastings <rickihastings22@gmail.com>
2014-01-07 17:18:41 +0000

    Changes to the message template parsing so it doesn't get re-rendered on tab swaps etc

--- client/messages/messages.js
+++ client/messages/messages.js
@@ -6,15 +6,14 @@ Template.messages.getMessages = function() {
 	if (this.type == 'network') {
 		return Events.find({network: this.name, target: null}, {sort: {'message.time': -1}});
 	} else {
-		var networkName = Networks.findOne({_id: this.network}).name;
-
-		return Events.find({network: networkName, target: this.target}, {sort: {'message.time': -1}});
+		//var networkName = Networks.findOne({_id: this.network}).name;
+		return Events.find({network: 'freenode', target: this.target}, {sort: {'message.time': -1}});
 	}
 };
 
-Template.messages.parseEvent = function() {
-	console.log(this);
-
-	return Template[this.type];
+Template.messages.typeEquals = function(type, equals) {
+	if (type == equals) {
+		return true;
+	}
 };
 // ----------------------------
