32f7cde4973ec2d0529386d85f380f2fee74770c
Ricki Hastings <rickihastings22@gmail.com>
2014-01-09 18:41:47 +0000

    3 hours of debugging to putting it down to being forced to add an artificial timeout because DOM manipulations are so slow. FUCK, YOU

--- client/messages/messages.js
+++ client/messages/messages.js
@@ -2,6 +2,21 @@
 // Template.messages
 // - everything inside .backlog (this is all the messages)
 
+Template.messages.rendered = function() {
+	var el = this.find('.inside-backlog'),
+		heightChanged = function() {
+			console.log('height has changed');
+		};
+
+	Meteor.setTimeout(function() {
+		new ResizeSensor(el, heightChanged);
+	}, 100);
+	// XXX - FUCK YOU METEOR. I spent 3 hours debugging this, tearing everything apart to try and
+	//       get it to work, and I'm resorting to putting in a timer, onload, ready, nothing works BUT this
+
+	heightChanged();
+};
+
 Template.messages.getMessages = function() {
 	if (this.type == 'network') {
 		return Events.find({network: this.name, target: null}, {sort: {'message.time': 1}});
