fef32e02bb96f497bb79de003905f55fe22b1ff5
Ricki Hastings <rickihastings22@gmail.com>
2014-01-11 16:07:14 +0000

    Minor userlist changes

--- client/userlist/userlist.js
+++ client/userlist/userlist.js
@@ -2,6 +2,14 @@
 // Template.userlist
 // - the userlist template
 
+Template.userlist.rendered = function() {
+	console.log('userlist rendered');
+};
+
+Template.userlist.destroyed = function() {
+	console.log('userlist destroyed');
+};
+
 Template.userlist.userCount = function() {
 	var name = Networks.findOne({_id: this.network}, {fields: {name: 1}}).name;
 	return ChannelUsers.find({network: name, channel: this.target}).count();
