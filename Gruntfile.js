9e9f2375e1f39160952f86ed1f1ea09962ebee10
Ricki Hastings <rickihastings22@gmail.com>
2014-04-08 16:34:30 +0000

    Adding source maps for better error logging. Needs latest grunt-contrib-uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,7 +38,8 @@ module.exports = function(grunt) {
 		},
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+				sourceMap: true
 			},
 			dependency: {
 				src: 'client/build/dependency.js',
