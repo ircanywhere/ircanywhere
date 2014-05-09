dd413283d2a5ce9ba06870056ffbb356bbbf1158
Ricki Hastings <rickihastings22@gmail.com>
2014-05-09 18:27:55 +0000

    Removing fibrous middleware, it's not working properly, manually doing it

--- server/app.js
+++ server/app.js
@@ -397,16 +397,13 @@ Application.prototype.setupServer = function() {
 	};
 	// error handling
 
-
 	app.use(express.compress());
 	app.use(express.static('client', {maxAge: 86400000}));
 	app.use(express.cookieParser(this.nodeId));
 	app.use(express.json());
 	app.use(express.urlencoded());
-	app.use(fibrous.middleware);
 	app.use(app.router);
 	app.use(error);
-
 	
 	// setup middleware
 
--- server/websocket.js
+++ server/websocket.js
@@ -136,15 +136,17 @@ WebSocket.prototype.sendBurst = function(data) {
 		path = '/api/burst/' +  helper.generateSalt(25);
 
 	application.app.get(path, function(req, res) {
-		var user = userManager.isAuthenticated(req.headers.cookie);
+		fibrous.run(function() {
+			var user = userManager.isAuthenticated(req.headers.cookie);
 
-		if (user._id.toString() === self._user._id.toString()) {
-			res.header('Content-Type', 'application/json');
-			res.end(JSON.stringify(data));
-		} else {
-			res.header('Content-Type', 'application/json');
-			res.end(JSON.stringify({'error': 'unauthenticated'}));
-		}
+			if (user._id.toString() === self._user._id.toString()) {
+				res.header('Content-Type', 'application/json');
+				res.end(JSON.stringify(data));
+			} else {
+				res.header('Content-Type', 'application/json');
+				res.end(JSON.stringify({'error': 'unauthenticated'}));
+			}
+		}, application.handleError.bind(application));
 	});
 	// generate a temporary route
 
