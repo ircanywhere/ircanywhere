736c47e1d7e684fad61dac48af24bc2ba763b9df
Ricki Hastings <rickihastings22@gmail.com>
2014-07-15 18:37:04 +0000

    Reverting changes in 9b56be0

--- server/app.js
+++ server/app.js
@@ -436,7 +436,7 @@ Application.prototype.setupServer = function() {
 	// error handling
 
 	app.use(express.compress());
-	app.use(express.static('client', {maxAge: 86400000}));
+	app.use(express.static('client/build', {maxAge: 86400000}));
 	app.use(express.cookieParser(this.nodeId));
 	app.use(express.json());
 	app.use(express.urlencoded());
@@ -444,9 +444,9 @@ Application.prototype.setupServer = function() {
 	app.use(error);
 	// setup middleware
 
-	app.get(/^\/(?!api|static|build\/(.*)).*$/, function(req, res) {
-		res.sendfile('./client/static/index.html');
-	});
+	// app.get(/^\/(?!api|static|build\/(.*)).*$/, function(req, res) {
+	// 	res.sendfile('./client/static/index.html');
+	// });
 	// setup routes
 
 	sockjsServer.on('connection', rpcHandler.onSocketOpen.bind(rpcHandler));
