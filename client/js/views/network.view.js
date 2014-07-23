e61899f1fa60451e1c5be60a183eae43fe051afa
Ricki Hastings <rickihastings22@gmail.com>
2014-07-23 19:20:23 +0000

    Actual fix for #112

--- client/js/views/network.view.js
+++ client/js/views/network.view.js
@@ -9,8 +9,8 @@ App.NetworkView = Ember.View.extend({
 	}.property('context.selectedTab.type').cacheable(),
 
 	showUserList: function() {
-		return (this.get('context.selectedTab.type') === 'channel' && this.get('context.selectedTab.active'));
-	}.property('context.selectedTab.type', 'context.selectedTab.active').cacheable(),
+		return (this.get('context.selectedTab.type') === 'channel' && this.get('context.selectedTab.active') && !this.get('context.selectedTab.hiddenUsers'));
+	}.property('context.selectedTab.type', 'context.selectedTab.active', 'context.selectedTab.hiddenUsers').cacheable(),
 
 	tabId: function() {
 		return 'tab-' + this.get('context.selectedTab._id');
