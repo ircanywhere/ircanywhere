0dc4f569d5e26385a5701609fc0369bbaad70736
Ricki Hastings <rickihastings22@gmail.com>
2014-09-19 17:11:06 +0000

    Fixing a bug where an empty networkRestriction config array wont accept any new connections. Also fixed a UF8 encoding issue with multibyte channel names. Closes #217

--- server/networks.js
+++ server/networks.js
@@ -303,8 +303,10 @@ NetworkManager.prototype.addNetworkApi = function(req) {
 		})
 		.then(function(user) {
 			restriction.forEach(function(item) {
-				var regex = helper.escape(item).replace(/\\\*/g, '(.*)');
-				escapedRestrictions.push(new RegExp('(' + regex + ')', 'i'));
+				if (item && item.trim() !== '') {
+					var regex = helper.escape(item).replace(/\\\*/g, '(.*)');
+					escapedRestrictions.push(new RegExp('(' + regex + ')', 'i'));
+				}
 			});
 			// create an array of restrictions
 
@@ -333,7 +335,7 @@ NetworkManager.prototype.addNetworkApi = function(req) {
 					output.errors.push({error: 'You have reached the maximum network limit of ' + application.config.clientSettings.networkLimit + ' and may not add anymore'});
 				}
 
-				var restricted = true;
+				var restricted = (restriction.length) ? true : false;
 				_.each(escapedRestrictions, function(item) {
 					if (item.test(server)) {
 						restricted = false;
@@ -341,7 +343,7 @@ NetworkManager.prototype.addNetworkApi = function(req) {
 				});
 
 				if (restricted) {
-					output.errors.push({error: 'There is a restriction inplace limiting your connections to ' + restriction});
+					output.errors.push({error: 'There is a restriction in place limiting your connections to ' + restriction});
 				}
 
 				if (output.errors.length > 0) {
@@ -416,8 +418,10 @@ NetworkManager.prototype.editNetworkApi = function(req) {
 		})
 		.then(function(user) {
 			restriction.forEach(function(item) {
-				var regex = helper.escape(item).replace(/\\\*/g, '(.*)');
-				escapedRestrictions.push(new RegExp('(' + regex + ')', 'i'));
+				if (item && item.trim() !== '') {
+					var regex = helper.escape(item).replace(/\\\*/g, '(.*)');
+					escapedRestrictions.push(new RegExp('(' + regex + ')', 'i'));
+				}
 			});
 			// create an array of restrictions
 
@@ -451,7 +455,7 @@ NetworkManager.prototype.editNetworkApi = function(req) {
 					output.errors.push({error: 'The port you have entered is invalid'});
 				}
 
-				var restricted = true;
+				var restricted = (restriction.length) ? true : false;
 				_.each(escapedRestrictions, function(item) {
 					if (item.test(server)) {
 						restricted = false;
